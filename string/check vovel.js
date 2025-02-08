3f7e7519e4cfa50836c3bff44ed36bb6da44237e
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-02-08 10:59:21 +0530

    javascript

--- string/check vovel.js
+++ string/check vovel.js
@@ -1,14 +1,14 @@
 
 function checkstring(str) {
 
-    let n=str.length;  
-    
-    for (let i = 0; i < n; i++) {
-        if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u")
-            return true;
-    }
-    
-    return false;
-    }
-    
-    console.log(checkstring("red"));
+let n=str.length;  
+
+for (let i = 0; i < n; i++) {
+    if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u")
+        return true;
+}
+
+return false;
+}
+
+console.log(checkstring("red"));
