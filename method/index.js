d7b9cbbfca91fcdcd645a51ebbffac7ba7e6e7f2
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-02-22 10:20:00 +0530

    js

---
+++ method/index.js
@@ -0,0 +1,12 @@
+let a=10
+let b=20
+console.log(Math.max(a,b));
+console.log(Math.min(a,b));
+console.log(Math.floor(5.8));
+console.log(Math.ceil(5.1));
+console.log(Math.round(5.1));
+console.log(Math.random()*10000);
+console.log(Math.round(Math.random()*10000));
+console.log(Math.abs(-100));
+console.log(Math.sqrt(49));
+console.log(Math.pow(2,3));
