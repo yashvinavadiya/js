1264513217670387bcdc0c57210eea80d93acd23
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-02-22 09:50:07 +0530

    js

--- dise/index.js
+++ dise/index.js
@@ -1,16 +1,12 @@
-function rollDice() {
-    const diceFaces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
-    let roll1 = Math.floor(Math.random() * 6);
-    let roll2 = Math.floor(Math.random() * 6);
-  
-    document.getElementById("player1").textContent = diceFaces[roll1];
-    document.getElementById("player2").textContent = diceFaces[roll2];
-  
-    if (roll1 > roll2) {
-      document.getElementById("result").textContent = "Player 1 Wins!";
-    } else if (roll2 > roll1) {
-      document.getElementById("result").textContent = "Player 2 Wins!";
-    } else {
-      document.getElementById("result").textContent = "It's a Tie!";
-    }
-  }
+document.getElementById("rollbutton").addEventListener("click",gamestart);
+
+function gamestart(){
+const randomdise= getrandomnumber();
+let imagetag= document.getElementById("diseimage");
+imagetag.src=`img/dise${randomdise}.png`;
+
+}
+
+function getrandomnumber(){
+    return Math.ceil(Math.random()*6)
+}
