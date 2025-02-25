5572c69f7e885cc337ad7a1c7651b1d67a53682a
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-02-25 17:20:32 +0530

    js

---
+++ settimeout/index.js
@@ -0,0 +1,8 @@
+setTimeout(()=>{
+    console.log("a");
+    console.log("b");
+    setTimeout(()=>{
+        console.log("yashvi");
+    }, 3000);
+    console.log("c");
+});
---
+++ settimeout/index2.js
@@ -0,0 +1,11 @@
+setInterval(()=>{
+    console.log("a");
+    console.log("b");
+    setTimeout(()=>{
+        console.log("yashvi");
+
+    },9000);
+    console.log("c");
+
+
+});
