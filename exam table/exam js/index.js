08cc774e2d6230ff2a685ab67c6891751da66562
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-02-25 10:47:14 +0530

    js

--- exam table/exam js/index.js
+++ exam table/exam js/index.js
@@ -38,17 +38,20 @@ const uimaker =()=>{
         td6.innerHTML=employ.experience;
 
         let td7=document.createElement("td");
-        td7.innerHTML=employ.sallery;
+        td7.innerHTML=employ.Sallery;
+
+        let td8=document.createElement("td");
+        td8.innerHTML=employ.experiencelevel;
 
         let button=document.createElement("button");
         button.addEventListener("click",()=>handeldelete(i))
         button.innerHTML="Delete";
 
-        let td8=document.createElement("td");
-        td8.append(button);
+        let td9=document.createElement("td");
+        td9.append(button);
 
         let tr=document.createElement("tr");
-        tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
+        tr.append(td1,td2,td3,td4,td5,td6,td7,td8,td9);
         document.getElementById("tableBody").append(tr);
 
 
@@ -56,31 +59,48 @@ const uimaker =()=>{
     });
 };
 
-const handelsubmit =(e) =>{
-    e.preventDefault();
 
-    let img=document.getElementById("img").value;
-    let name=document.getElementById("name").value;
-    let number=document.getElementById("number").value;
-    let email=document.getElementById("email").value;
-    let jobRoll=document.getElementById("jobRoll").value;
-    let experience=document.getElementById("experience").value;
-    let Sallery=document.getElementById("Sallery").value;
+    document.getElementById("newEmploy").addEventListener("submit", function  handelsubmit (e) {
+        e.preventDefault();
+    
+        let img=document.getElementById("img").value;
+        let name=document.getElementById("name").value;
+        let number=document.getElementById("number").value;
+        let email=document.getElementById("email").value;
+        let jobRoll=document.getElementById("jobRoll").value;
+        let experience=document.getElementById("experience").value;
+        let Sallery=document.getElementById("Sallery").value;
+        let experiencelevel="";
+        if(experience<3){
+            experiencelevel="Junior";
+        }
+        else if(experience>=3 && experience<5){
+            experiencelevel="middle";
+        }
+        else{
+            experiencelevel="Senior";
+        }
 
-    let newEmploy={
-        img:img,
-        name:name,
-        number:number,
-        email:email,
-        jobRoll:jobRoll,
-        experience:experience,
-        Sallery:Sallery
-    };
-
-    arr.push(newEmploy);
-
-    uimaker();
-
-};
-    document.getElementById("newEmploy").addEventListener("submit",handelsubmit);
+        console.log(experience);
+        if(experience>1 && experience<3){
+            console.log("junior");
+        }
+    
+        let newEmploy={
+            img:img,
+            name:name,
+            number:number,
+            email:email,
+            jobRoll:jobRoll,
+            experience:experience,
+            Sallery:Sallery,
+            experiencelevel:experiencelevel
+        };
+    
+        arr.push(newEmploy);
+    
+        uimaker();
+    
+    }
+);
    
