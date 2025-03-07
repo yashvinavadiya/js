ee031b0b878ed3c619fcd05526d587c66e9ee101
yashvinavadiya <navadiyayashvi72@gmail.com>
2025-03-07 15:26:58 +0530

    js

--- sign up form/index.js
+++ sign up form/index.js
@@ -1,28 +1,27 @@
-const getvalue=(id)=>{
-    return document.getElementById(id).value
-}
+const getvalue = (id) => {
+  return document.getElementById(id).value;
+};
+
 const validateInput = (id, regex) => {
-    let value = document.getElementById(id).value;
-    let isValid = regex.test(value);
-    document.getElementById(id).setAttribute("class", isValid ? "paased" : "err");
-  };
-  
-const handleuserData=(e)=>{
-    e.preventDefault();
+  let value = document.getElementById(id).value;
+  let isValid = regex.test(value);
+  document.getElementById(id).setAttribute("class", isValid ? "passed" : "err");
+};
+
+const handleuserData = (e) => {
+  e.preventDefault();
+
+  let name = getvalue("name");
+  let email = getvalue("email");
+  let number = getvalue("number");
+  let password = getvalue("password");
 
 
-    let name=getvalue("name").value;
-    let email=getvalue("email").value;
-    let number=getvalue("number").value;
-    let password=getvalue("password").value;
+  validateInput("name", /^[A-Za-z\s]{1,50}$/);
+  validateInput("email", /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  validateInput( "password",/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!#$%&?])[A-Za-z\d!#$%&?]{8,20}$/ );
+  validateInput("number", /^[789]\d{9}$/);
 
-  validateInput("name", /^(?=.*[0-9])(?=.*[A-Za-z]).{8,32}$/); 
-  validateInput("email", /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/); 
-  validateInput(
-    "password",
-    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/
-  ); 
-  validateInput("number", /^\d{10}$/);
+};
 
-}
-document.getElementById("userdata").addEventListener("submit",handleuserData);
+document.getElementById("userdata").addEventListener("submit", handleuserData);
